d3aec94e91279dbf8215827d358a90ef4c6fc21d
Пользователь <eightshaman@gmail.com>
2021-12-07 23:40:30 +0300

    Добавил тесты для проверки ссылок для промоакции promo=offer0 - promo=offer9

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,11 @@
 from .pages.product_page import PageObject
 from .pages.base_page import BasePage
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('offer', ["promo=offer0", "promo=offer1", "promo=offer2", "promo=offer3", "promo=offer4", "promo=offer5", "promo=offer6", "promo=offer7", "promo=offer8", "promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?{offer}"
     page = PageObject(browser, link)
     page.open()
     page.book_add_to_bucket()
