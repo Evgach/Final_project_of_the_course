1f01b5bc67e5a3262216b9610b75a9f137faba9c
Пользователь <eightshaman@gmail.com>
2021-12-08 00:39:29 +0300

    Пометил падающий тест как xfail

--- test_product_page.py
+++ test_product_page.py
@@ -3,9 +3,9 @@ from .pages.base_page import BasePage
 import time
 import pytest
 
-@pytest.mark.parametrize('offer', ["promo=offer0", "promo=offer1", "promo=offer2", "promo=offer3", "promo=offer4", "promo=offer5", "promo=offer6", "promo=offer7", "promo=offer8", "promo=offer9"])
+@pytest.mark.parametrize('offer', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
 def test_guest_can_add_product_to_basket(browser, offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?{offer}"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=offer{offer}"
     page = PageObject(browser, link)
     page.open()
     page.book_add_to_bucket()
@@ -14,3 +14,7 @@ def test_guest_can_add_product_to_basket(browser, offer):
     browser.implicitly_wait(1)
     page.message_about_item_added_to_basket()
     page.message_about_price_of_the_basket()
+    
+@pytest.mark.parametrize('offer', [*range(0,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser, offer):
+   link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}'
