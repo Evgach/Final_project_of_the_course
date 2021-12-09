79097d28b51f597e9f7af96555e1165139e469ce
Пользователь <eightshaman@gmail.com>
2021-12-10 00:24:31 +0300

    Добавил кейсы по уроку плюсы наследования

--- pages/locators.py
+++ pages/locators.py
@@ -21,3 +21,8 @@ class ProductPageLocators():
     PRICE_ON_PAGE = (By.CSS_SELECTOR, ".col-sm-6.product_main>.price_color")
     
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
+    
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,7 @@ def test_guest_can_add_product_to_basket(browser, offer):
 def test_guest_can_add_product_to_basket(browser, offer):
    link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=offer{offer}"
 
-#@pytest.mark.skip   
+@pytest.mark.skip   
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
     page = PageObject(browser, link)
@@ -29,13 +29,15 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.book_add_to_basket()
     #browser.implicitly_wait(1)
     page.should_not_be_success_message()
-
+    
+@pytest.mark.skip 
 def test_guest_cant_see_success_message(browser): 
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
     page = PageObject(browser, link)
     page.open()
     page.should_not_be_success_message()
     
+@pytest.mark.skip     
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
     page = PageObject(browser, link)
@@ -43,3 +45,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.book_add_to_basket()
     #browser.implicitly_wait(1)
     page.should_not_be_success_message_is_disappeared()
+
+#@pytest.mark.skip    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+#@pytest.mark.skip    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = PageObject(browser, link)
+    page.open()
+    browser.implicitly_wait(1)
+    page.go_to_login_page()
